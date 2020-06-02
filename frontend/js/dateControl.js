4d695362673a307a03a7554d8fd8f57c5f03d25b
sisifo <jl.durand95@gmail.com>
2020-06-01 23:30:43 -0400

    gitUpdate

--- frontend/js/dateControl.js
+++ frontend/js/dateControl.js
@@ -1,6 +1,6 @@
 
 // filtrarFechas("fechaRaw", rawDate, jsonData);
-const filtrarFechas = (fecha, json, op) => {
+const filtrarFechas = (fecha, fechaReciente, fechaAntigua, json, op) => {
     console.log(fecha);
     let tempList = new Array();
     switch(op){
@@ -12,10 +12,9 @@ const filtrarFechas = (fecha, json, op) => {
             });
             break;
         case -1:
-            // aquí buscamos la siguiente fecha (meno, vamos en orden descendente)
-            // debemos validar que exista fecha siguiente, pues podríamos estar
-            // en el último campo (más antiguo)
-            let flag = 0;
+            if(fecha == fechaAntigua){
+                return fechaAntigua;
+            }
             json.forEach(obj => {
                 if(obj.fecha.slice(0, 10) == fecha && flag == 0){
                     flag = 1;        // when this even occurs, 
@@ -33,30 +32,51 @@ const filtrarFechas = (fecha, json, op) => {
                     }
                 }
             });
-            break; // just in case
+            return fecha;
         case  1:
-            break;
+            if(fecha == fechaReciente){
+                return fechaReciente;
+            }
+            for(let i = json.length-1; i >= 0; i--){
+                if(json[i].fecha.slice(0, 10) == fecha && flag == 0){
+                    flag = 1;        // when this even occurs, 
+                }else if(flag == 1){ // the are positionated in the correct date so:
+                    if(json[i].fecha.slice(0, 10) != fecha && flag == 1){
+                        // encontramos el siguiente
+                        fecha = json[i].fecha.slice(0, 10); // almacenamos nueva fecha
+                        flag = 2;
+                        tempList.append(json[i]); // almacenamos primer ocurrencia
+                }else if(flag == 2){
+                    if(json[i].fecha.slice(0,10) == fecha){
+                        tempList.append(json[i]);
+                    }else{
+                        break;
+                    }
+                }
+            }
+            return fecha;
     }
     return fecha;
 }
 (function($){
     $(document).ready(async()=>{
         fechaReciente, fechaAntigua, jsonData = await obtenFecha();
-        fecha = fecha.slice(0, 10);
-        fecha = filtrarFechas(fechaReciente, fechaAntigua, jsonData, 0);      // fill & filter
-        $('.left').click(() => {
-            fecha = filtrarFechas(fechaReciente, fechaAntigua, jsonData, -1); // -1 get prev register
+        fechaReciente = fechaReciente.slice(0, 10);
+        fechaAntigua  = fechaAntigua.slice(0, 10);
+        fecha         = filtrarFechas(fechaReciente, fechaAntigua, jsonData, 0);
+        $('.left').click(()  => {
+            fecha = filtrarFechas(fecha, fechaReciente, fechaAntigua, jsonData, -1);
         });
         $('.right').click(() => {
-            fecha = filtrarFechas(fecha, jsonData,  1); // +1
+            fecha = filtrarFechas(fecha, fechaReciente, fechaAntigua, jsonData, 1);
         });
     });
 })(jQuery);
 
 const obtenFecha = async() => {
     const ret          = new pedidosFecha();
-    const jsonData     = await ret.listar();                // recibimos todo
-    let   recentField  = jsonData[0].fecha;                 // fecha más reciente
+    const jsonData     = await ret.listar();                 // recibimos todo
+    let   recentField  = jsonData[0].fecha;                  // fecha más reciente
     let   lastField    = jsonData[jsonData.length-1].fecha;
     return { recentField, lastField, jsonData };             // enviamos fecha en bruto y los registros
 }
