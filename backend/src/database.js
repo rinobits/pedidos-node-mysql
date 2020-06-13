2ae9ab672d20102df75b2adea880013eac3956c9
rinobits <59568971+rinobits@users.noreply.github.com>
2020-06-12 23:53:18 -0400

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
   host: '138.197.7.205',
   user: 'root',
-  password: 'M41gn4c14_',
+  password: '',
   database: 'jean001',
   multipleStatements: true
 });
